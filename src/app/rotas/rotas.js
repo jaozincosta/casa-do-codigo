e89539248287f01233e01156537d68d944595667
João Costa <138248786+jaozincosta@users.noreply.github.com>
2025-05-07 23:28:38 -0300

    feat: novo form de inserção de livro

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -18,17 +18,21 @@ module.exports = (app) => {
 
   app.get("/livros", function (req, resp) {
     const livroDao = new LivroDao(db);
-    livroDao.lista()
+    livroDao
+      .lista()
       .then((livros) =>
         resp.marko(require("../views/livros/lista/lista.marko"), {
           livros: livros,
         })
       )
       .catch((erro) => console.log(erro));
-    /* /*  livroDao.lista(function (erro, resultados) {
-      resp.marko(require("../views/livros/lista/lista.marko"), {
-        livros: resultados,
-      }); 
-    }); */
+  });
+
+  app.get("/livros/form", function (req, resp) {
+    resp.marko(require("../views/livros/form/form.marko"));
+  });
+
+  app.post("/livros", function (req, resp) {
+    console.log(req.body);
   });
 };
