2fb7960f4bcb3917b86751f20797fdbb420d8c03
João Costa <138248786+jaozincosta@users.noreply.github.com>
2025-05-09 15:27:41 -0300

    feat: configura middleware com body-parser para lidar com requisições POST

--- src/config/custom-express.js
+++ src/config/custom-express.js
@@ -1,10 +1,17 @@
-require('marko/node-require').install();
-require('marko/express');
+require("marko/node-require").install();
+require("marko/express");
 
-const express = require('express');
+const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
-const rotas = require('../app/rotas/rotas');
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+const rotas = require("../app/rotas/rotas");
 rotas(app);
 
 module.exports = app;
