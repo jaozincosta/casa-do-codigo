2f466abce1032a46d550f3b055bb2791dac9cd55
João Costa <138248786+jaozincosta@users.noreply.github.com>
2025-05-10 16:00:22 -0300

    feat: "Implementa rotas PUT e DELETE com method-override, variáveis de URL e AJAX"

--- src/app/infra/livro-dao.js
+++ src/app/infra/livro-dao.js
@@ -7,12 +7,12 @@ class LivroDao {
     return new Promise((resolve, reject) => {
       this._db.run(
         `
-            INSERT INTO LIVROS (
-                    titulo,
-                    preco,
-                    descricao
-                ) values (?, ?, ?)
-            `,
+        INSERT INTO livros (
+          titulo,
+          preco,
+          descricao
+        ) VALUES (?, ?, ?)
+        `,
         [livro.titulo, livro.preco, livro.descricao],
         function (err) {
           if (err) {
@@ -30,8 +30,57 @@ class LivroDao {
     return new Promise((resolve, reject) => {
       this._db.all("SELECT * FROM livros", (erro, resultados) => {
         if (erro) return reject("Não foi possível listar os livros!");
-
         return resolve(resultados);
+      });
+    });
+  }
+
+  buscaPorId(id) {
+    return new Promise((resolve, reject) => {
+      this._db.get(
+        "SELECT * FROM livros WHERE id = ?",
+        [id],
+        (erro, resultado) => {
+          if (erro) {
+            return reject("Não foi possível encontrar o livro!");
+          }
+          return resolve(resultado);
+        }
+      );
+    });
+  }
+
+  atualiza(livro) {
+    return new Promise((resolve, reject) => {
+      this._db.run(
+        `
+        UPDATE livros 
+        SET 
+          titulo = ?, 
+          preco = ?, 
+          descricao = ?
+        WHERE id = ?
+        `,
+        [livro.titulo, livro.preco, livro.descricao, livro.id],
+        function (erro) {
+          if (erro) {
+            return reject("Não foi possível atualizar o livro!");
+          }
+
+          return resolve();
+        }
+      );
+    });
+  }
+
+  remove(id) {
+    return new Promise((resolve, reject) => {
+      this._db.run(`DELETE FROM livros WHERE id = ?`, [id], function (erro) {
+        if (erro) {
+          return reject("Não foi possível remover o livro!");
+        }
+
+        return resolve();
       });
     });
   }
